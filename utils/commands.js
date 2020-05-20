149a0a3ff5ca9d8f672afb16285ee78b2aef0932
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 14:41:08 +1000

    Seperating the handling of commands
    Commands will now be in their own file called `Commands`.
    
    This is so we can have more advanced logic in them, in a neater fashion.

---
+++ utils/commands.js
@@ -0,0 +1,31 @@
+import chalk from 'chalk';
+import { NOT_ENOUGH_COORDINATES, NOT_VALID_DIRECTION_NAME } from './errors';
+import { placeRobot, moveRobot } from './map';
+
+const log = console.log;
+const error = chalk.bgRed;
+
+export const LEFT = (currentDirection, directionList) => {
+	let currentDirectionPosition = directionList.indexOf(
+		currentDirection.toUpperCase()
+	);
+	if (currentDirectionPosition > 0) {
+		return directionList[currentDirectionPosition - 1];
+	} else {
+		return directionList[3];
+	}
+};
+
+export const RIGHT = (currentDirection, directionList) => {
+	let currentDirectionPosition = directionList.indexOf(
+		currentDirection.toUpperCase()
+	);
+
+	if (currentDirectionPosition < 3) {
+		return directionList[currentDirectionPosition + 1];
+	} else {
+		return directionList[0];
+	}
+};
+
+export const REPORT = (map) => {};
