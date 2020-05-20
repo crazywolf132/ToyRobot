383f08fb4c253436e36dd09fb9bef24f12748f08
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 15:30:07 +1000

    Adding a never ending prompt
    This will allow us to keep taking the user input

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { createInterface } from 'readline';
 import { generateMap } from './utils/map';
 import chalk from 'chalk';
 
+// Setting up logger and readLine
 const log = console.log;
 const rl = createInterface({
 	input: process.stdin,
@@ -12,3 +13,17 @@ const _ValidCommands = ['PLACE', 'MOVE', 'LEFT', 'RIGHT', 'REPORT'];
 const _Map = generateMap();
 let firstMove = true;
 log(_Map);
+const prompt = () => {
+	rl.question('> ', (userInput) => {
+		handleInput(userInput);
+		prompt(); // Calling this function again to recursively run.
+	});
+};
+
+const StartGame = () => {
+	// This is used to generate the map, then prompt the user...
+	_Map = generateMap();
+	prompt();
+};
+
+StartGame();
