dcc318948cd20382eeba65363c85f81191419061
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 15:32:10 +1000

    Adding some more starting variables
    These variables are used to run the game.
    
    - _Directions : keeps a list of the valid directions.
    - _Map : keeps the current version of the map, and where the toy robot is.
    - currentDirection : Keeps the current direction the robot is facing

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ const rl = createInterface({
 	output: process.stdout,
 });
 rl.setPrompt('> ');
+
+// Setting up variables that should never be changed. Hence the '_' prefix;
 const _ValidCommands = ['PLACE', 'MOVE', 'LEFT', 'RIGHT', 'REPORT'];
-const _Map = generateMap();
+const _Directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+// This is the only exception... As we actively modify the map by our system...
+let _Map;
+
+// Used to store the Current position. Ultimately, this could be an INT, and the LIST be an ENUM
+// But as there is only 4 possible choices, it simply isnt worth it.
+let currentDirection = '';
+// Used to work out if we need to ignore other commands or not.
 let firstMove = true;
-log(_Map);
 const prompt = () => {
 	rl.question('> ', (userInput) => {
 		handleInput(userInput);
