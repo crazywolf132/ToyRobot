e3e5e9c356a0876f59381973acd1e6bc8cba0563
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 14:33:05 +1000

    Updating how the map is filled
    Using the Array class to fill a map with `0`s
    
    This will mean we can just search for a `1`, and that is where our robot will be.

--- utils/map.js
+++ utils/map.js
@@ -1,10 +1,14 @@
-export const generateMap = () => {
-	let startingMap = [];
-	startingMap.length = 5;
+import chalk from 'chalk';
+import { BROKEN_THE_GAME_ERROR } from './errors';
 
-	startingMap.forEach((row) => {
-		row = [];
-		row.length = 5;
+const log = console.log;
+const error = chalk.bgRed;
+
+export const generateMap = () => {
+	let startingMap = new Array(5).fill([]);
+
+	startingMap = startingMap.map((row) => {
+		row = new Array(5).fill(0);
 		return row;
 	});
 	return startingMap;
