51c1be201ff2c85baf9f0f3250ed8b9b983cabec
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 15:33:15 +1000

    Added core input handler
    This function re-routes the userInput to the correct command and tells the game what to do.
    
    It will also check if it is the first move and handle it.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { createInterface } from 'readline';
 import { generateMap } from './utils/map';
 import chalk from 'chalk';
+import { BROKEN_THE_GAME_ERROR } from './utils/errors';
+import { PLACE, MOVE, LEFT, RIGHT, REPORT } from './utils/commands';
 
 // Setting up logger and readLine
 const log = console.log;
@@ -21,6 +23,55 @@ let _Map;
 let currentDirection = '';
 // Used to work out if we need to ignore other commands or not.
 let firstMove = true;
+
+const handleInput = (userInput) => {
+	if (firstMove) {
+		if (!userInput.toUpperCase().includes('PLACE')) {
+			return log(
+				chalk.bgRed('Please use the PLACE command to start the game!')
+			);
+		}
+	}
+
+	// Checking to see if we have a valid command...
+	// Getting the starting of the command
+	let command = userInput.split(' ')[0].toUpperCase();
+	if (!_ValidCommands.includes(command))
+		return log(chalk.bgRed('Please use a valid command.'));
+
+	// As we are here... we assume we are now working with a valid command...
+	// We just need to workout which one...
+
+	switch (command) {
+		case 'PLACE':
+			let result = PLACE(_Map, _Directions, currentDirection, userInput);
+			if (result !== -1 && firstMove) {
+				firstMove = false;
+			}
+			// Simple turnary to only change the map if we returned a new map.
+			_Map = result.map ? result.map : _Map;
+			currentDirection = result.currentDirection
+				? result.currentDirection
+				: currentDirection;
+			break;
+		case 'MOVE':
+			_Map = MOVE(_Map, currentDirection);
+			break;
+		case 'LEFT':
+			currentDirection = LEFT(currentDirection, _Directions);
+			break;
+		case 'RIGHT':
+			currentDirection = RIGHT(currentDirection, _Directions);
+			break;
+		case 'REPORT':
+			REPORT(_Map, currentDirection);
+			break;
+		default:
+			// There should not be a conceivable way of getting here... but just incase, lets handle it.
+			return log(chalk.bgYellow(BROKEN_THE_GAME_ERROR));
+	}
+};
+
 const prompt = () => {
 	rl.question('> ', (userInput) => {
 		handleInput(userInput);
