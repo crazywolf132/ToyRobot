53ee2c3a1f32e177ccfb23c7faeafbee0c8714fc
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 15:33:56 +1000

    Added the PLACE command
    This command is used to PLACE the robot anywhere inside the map.

--- utils/commands.js
+++ utils/commands.js
@@ -5,6 +5,37 @@ import { placeRobot, moveRobot, getPosition } from './map';
 const log = console.log;
 const error = chalk.bgRed;
 
+export const PLACE = (map, directionList, currentDirection, userInput) => {
+	// Checking for arguments
+	let commandList = userInput.split(' ');
+	if (commandList.length !== 2) {
+		log(error(NOT_ENOUGH_COORDINATES));
+		return -1;
+	}
+
+	// Checking that all the information is in the argument.
+	let postionInfo = commandList[1].toUpperCase().split(',');
+	if (postionInfo.length !== 3) {
+		log(error(NOT_ENOUGH_COORDINATES));
+		return -1;
+	}
+
+	// We are here, so this means the information provided is so far correct.
+	if (!directionList.includes(postionInfo[2])) {
+		log(error(NOT_VALID_DIRECTION_NAME));
+		return -1;
+	} else {
+		currentDirection = postionInfo[2];
+	}
+
+	let result = placeRobot(map, postionInfo[0], postionInfo[1]);
+	if (result !== -1) {
+		return { map, currentDirection };
+	} else {
+		return -1;
+	}
+};
+
 export const MOVE = (map, direction) => {
 	return moveRobot(map, direction);
 };
