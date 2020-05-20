636ec2063e9264d33bea40d5246e4a0afb8185e0
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 15:29:08 +1000

    Added a new error for invalid coordinates

--- utils/errors.js
+++ utils/errors.js
@@ -2,5 +2,6 @@ export const NOT_ENOUGH_COORDINATES = `Please provide 2 coordinates and a direct
 export const PLACE_COMMAND_REQUIRED = `Please use the PLACE command to start the game!`;
 export const NOT_VALID_COMMAND_NAME = `Please input a valid command.`;
 export const NOT_VALID_DIRECTION_NAME = `Please use a valid direction. NORTH, SOUTH, EAST, or WEST`;
+export const PLEASE_CHOOSE_A_VALID_X_Y = `Please provided a valid starting point between 0-5,0-5`;
 
 export const BROKEN_THE_GAME_ERROR = `Oh no... you seem to have broken me :(`;
