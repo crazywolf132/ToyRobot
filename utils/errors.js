48ff861cf07d4a128f36bea02bcecd6fa531edb7
crazywolf132 <crazywolf132@gmail.com>
2020-05-20 14:31:58 +1000

    Creating Error Messages

---
+++ utils/errors.js
@@ -0,0 +1,6 @@
+export const NOT_ENOUGH_COORDINATES = `Please provide 2 coordinates and a direction`;
+export const PLACE_COMMAND_REQUIRED = `Please use the PLACE command to start the game!`;
+export const NOT_VALID_COMMAND_NAME = `Please input a valid command.`;
+export const NOT_VALID_DIRECTION_NAME = `Please use a valid direction. NORTH, SOUTH, EAST, or WEST`;
+
+export const BROKEN_THE_GAME_ERROR = `Oh no... you seem to have broken me :(`;
